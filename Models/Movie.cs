57e56bcd409ae4f2a5f8ce201f7d8bd62386b867
Dipti Kartikeya <Dipti.Kartikeya@atcltd.atc.org.nz>
2021-02-25 13:20:51 +1300

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
